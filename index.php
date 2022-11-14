a6ac34ecb0eef9e532ced50f484e5b6e641a060a
CallMeRinChan <call0me0rin0chan@gmail.com>
2022-11-14 08:55:58 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<script> <!--Yes, I'll be using JS and PHP in this file, fuck you-->
+    
+</script>
+<html lang="en">
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+    <style>
+
+    </style>
+</head>
+<body>
+</body>
+</html>
